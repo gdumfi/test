667f09508aca2a644506ae45fbd953efe201ff95
yanchi <gdumfi@yandex.ru>
2026-02-05 10:29:09 +0300

    2

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('yana')
+    print_hi('yana_branch')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
